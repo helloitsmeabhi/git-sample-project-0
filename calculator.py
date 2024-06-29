00c6ebbc1ac68bd54fa125338dca29092aa12f1f
helloitsmeabhi <helloitsmeabhinav2003@gmail.com>
2024-06-29 15:01:30 +0530

    small Fix

--- calculator.py
+++ calculator.py
@@ -10,16 +10,16 @@ o=input("Enter operator : ")
 b=int(input("Enter number 2 : "))
 if b==0:
     print("Exception cannot divide by 0")
-
-if o[0] in [ '+','-','*','/' ]:
-    if o[0] == '+':
-        out = a + b
-    elif o[0] == '-':
-        out = a - b
-    elif o[0] == '*':
-        out = a * b
-    elif o[0] == '/':
-        out = a//b
-    print("Output : ",out)
 else:
-    print("Error : Invalid Operator")
+    if o[0] in [ '+','-','*','/' ]:
+        if o[0] == '+':
+            out = a + b
+        elif o[0] == '-':
+            out = a - b
+        elif o[0] == '*':
+            out = a * b
+        elif o[0] == '/':
+            out = a//b
+        print("Output : ",out)
+    else:
+        print("Error : Invalid Operator")
